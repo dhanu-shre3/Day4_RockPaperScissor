519294b99112b63be8811ec6ea28bfaa7b7600ff
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-05-22 21:25:35 +0100

    practice

---
+++ practice.py
@@ -0,0 +1,16 @@
+# names =  names_string.split(", ")
+
+# import random
+
+# num_items =  len(names)
+
+# random_choice = random.randit(0, num_items - 1)
+
+# print(names[random_choice])
+
+#nested lists
+fruits =  ["strawberries", "grapes", "peaches"]
+vegetables = ["potato", "spinach", "celery"]
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen [1][2])
